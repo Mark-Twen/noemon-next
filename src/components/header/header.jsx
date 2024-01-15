762b498a7691f1908b1ec905848a0fbfc5f1588a
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-01-15 20:26:35 +0200

    header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../../public/img/logo/LOGO-4x.png";
+import fonts from "../fonts/fonts.module.scss";
+import styles from "./header.module.scss";
+
+const Header = () => {
+  return (
+    <>
+      <section className="flex w-screen bg-logoBg layout">
+        {/* LOGO */}
+        <div className="py-8 ml-[3%] ">
+          <Image
+            src={logo}
+            alt="logo"
+            width={254}
+            height={81}
+            priority={true}
+          />
+        </div>
+
+        {/* Links */}
+
+        <div className={`flex py-[60px] ml-[40%]  ${fonts.linkcolor}`}>
+          {/* <Link className={`pr-[12%] ${fonts.linkcolor}`} href="">
+            Home
+          </Link> */}
+          <Link
+            className={`pr-[12%]  ${fonts.linkcolor} ${styles.link}`}
+            href=""
+          >
+            Services
+          </Link>
+          <Link
+            className={`pr-[12%] ${fonts.linkcolor} ${styles.link}`}
+            href="/productPage"
+          >
+            Products
+          </Link>
+          <Link
+            className={`pr-[12%] ${fonts.linkcolor} ${styles.link}`}
+            style={{ whiteSpace: "nowrap" }}
+            href=""
+          >
+            About us
+          </Link>
+          <Link
+            className={`pr-[12%] ${fonts.linkcolor} ${styles.link}`}
+            href=""
+          >
+            Contact
+          </Link>
+        </div>
+
+        {/* Contacts */}
+        <div className="flex flex-col pl-[15%] py-[36px] ">
+          <Link
+            className={`pb-3 ${fonts.linkcolor} ${styles.link}`}
+            href="tel:18005339263"
+          >
+            1-800-533-9263
+          </Link>
+          <Link
+            className={`${fonts.linkcolor} ${styles.link}`}
+            href="mailto:info@noemon.ca"
+          >
+            info@noemon.ca
+          </Link>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Header;
