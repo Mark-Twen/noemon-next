a8ecf93ec9bcb6e4760c8b749fb2c051b24b564d
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-01-16 11:43:55 +0200

    manage Images from Sanity

--- src/app/productPage/page.jsx
+++ src/app/productPage/page.jsx
@@ -1,10 +1,20 @@
 import { client } from "../../utils/configSanity";
 import { PortableText } from "@portabletext/react";
+import Image from "next/image";
+
+import { urlFor } from "../../utils/configSanity";
 
 import fonts from "../../components/fonts/fonts.module.scss";
+import { url } from "inspector";
 
 async function getData() {
-  const query = `*[_type == 'prodKit']`;
+  const query = `*[_type == 'prodKit'] {
+  title,
+  script,
+  description,
+  _id,
+  image
+}`;
   const data = await client.fetch(query);
   console.log(data);
   return data;
@@ -18,6 +28,14 @@ const productPage = async () => {
       <div>
         {data?.map((item) => (
           <div key={item?._id}>
+            <div>
+              <Image
+                src={urlFor(item.image).url()}
+                alt="product image card 1"
+                width={196}
+                height={196}
+              />
+            </div>
             <div>
               <span className={fonts.callExpertBtm}>{item.title}</span>
               <span className={fonts.callExpertBtm}>{item.script}</span>
