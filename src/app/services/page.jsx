c03889a00ad42d6a148bd1a256f370ac877f37f8
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-01-22 23:51:35 +0200

    123

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,12 +1,14 @@
 import Logon from "../../components/Logon/logon";
 
 import Hero from "../../components/services/heroServices"
+import RightCards from "../../components/bigGardsMaket/rigntCardsServices"
 
 const Services = () => {
   return (
     <div className="layout">
         <Hero />
       <Logon type="services" />
+      <RightCards type = "ElService" />
     </div>
   );
 };
