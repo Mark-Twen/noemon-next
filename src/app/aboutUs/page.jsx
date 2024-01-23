3069a871a9c40810469ff8040186b5904161deea
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-01-23 13:42:26 +0200

    about us card

--- src/app/aboutUs/page.jsx
+++ src/app/aboutUs/page.jsx
@@ -1,6 +1,7 @@
 
 import Hero from "../../components/aboutUsPageComponents/heroAboutUs"
 import Reviews from "../../components/aboutUsPageComponents/reviews"
+import Card from "../../components/aboutUsPageComponents/card"
 
 const AboutUs = () => {
     return (
@@ -8,6 +9,7 @@ const AboutUs = () => {
     <div className="layout">
         <Hero />
         <Reviews />
+        <Card />
     </div>
 
 )
