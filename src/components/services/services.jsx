46c6f5b51652b5193a6e21dc73178df14366d949
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-01-26 12:59:43 +0200

    AddMenu

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -14,36 +14,54 @@ import fonts from "../fonts/fonts.module.scss";
 const Services = () => {
   const [isSubMenuServices, setIsSubMenuServices] = useState(false);
   const [isSubMenuResidencial, setIsSubMenuResidencial] = useState(false);
-  const [isHovered, setIsHovered] = useState(false);
+  const [isSubMenuAuto, setIsSubMenuAuto] = useState(false);
+  const [leaveTimeout, setLeaveTimeout] = useState(null);
 
   const handleOnClick = () => {
     setIsSubMenuServices(!isSubMenuServices);
-    setIsSubMenuResidencial(!isSubMenuResidencial)
+    setIsSubMenuResidencial(!isSubMenuResidencial);
+    setIsSubMenuAuto(!isSubMenuAuto);
   };
 
   const handleOnMouseEnterServices = () => {
     setIsSubMenuServices(true);
-    
-  };
-  const handleOnMouseEnterResidancial = () => {   
-    setIsSubMenuResidencial(true);
+    setIsSubMenuResidencial(false);
+    setIsSubMenuAuto(false);
+    clearTimeout(leaveTimeout);
   };
 
-  const handleOnMouseLeaveServises = () => {   
+  const handleOnMouseEnterResidancial = () => {
+    setIsSubMenuResidencial(true);
     setIsSubMenuServices(false);
-  }
-  const handleOnMouseLeaveResidancial = () => {
+    setIsSubMenuAuto(false);
+    clearTimeout(leaveTimeout);
+  };
+  const handleOnMouseEnterAutomation = () => {
+    setIsSubMenuServices(false);
     setIsSubMenuResidencial(false);
-    
-  }
+    setIsSubMenuAuto(true);
+    clearTimeout(leaveTimeout);
+  };
+
+  const handleOnMouseLeaveServices = () => {
+    setLeaveTimeout(setTimeout(() => setIsSubMenuServices(false), 2000));
+  };;
+  const handleOnMouseLeaveResidancial = () => {
+    setLeaveTimeout(setTimeout(() => setIsSubMenuResidencial(false), 2000));
+  };
+
+  const handleOnMouseLeaveAutomation = () => {
+    setLeaveTimeout(setTimeout(() => setIsSubMenuAuto(false), 2000));
+  };
 
   return (
     <>
       <ul className="my-[120px] layout">
-        <li className={`${styles.list} ${styles.navLink}`}
-         onMouseEnter={handleOnMouseEnterServices}
-         onMouseLeave={handleOnMouseLeaveServises}
-        >        
+        <li
+          className={`${styles.list} ${styles.navLink}`}
+          onMouseEnter={handleOnMouseEnterServices}
+          onMouseLeave={handleOnMouseLeaveServices}
+        >
           <Link
             onClick={handleOnClick}
             href="services"
@@ -58,31 +76,58 @@ const Services = () => {
               className={` mt-[4px] ${styles.arrow}`}
             />
           </Link>
-
-          {isSubMenuServices && (
-            <ul className={`${styles.subNavLink} ${isHovered ? styles.hovered : ''}`}>
-              <li className={styles.subNavLinkItems}>
-                <Link href="/services/lightening">Lighting</Link>
-              </li>
-              <li className={styles.subNavLinkItems}>
-                <Link href="#">Menu-2</Link>
-              </li>
-              <li className={styles.subNavLinkItems}>
-                <Link href="#">Menu-3</Link>
-              </li>
-              <li className={styles.subNavLinkItems}>
-                <Link href="#">Menu-4</Link>
-              </li>
-            </ul>
-          )}
         </li>
+        {isSubMenuServices && (
+          <ul
+            className={`${styles.subNavLink}`}
+            onMouseEnter={handleOnMouseEnterServices}
+            onMouseLeave={handleOnMouseLeaveServices}
+          >
+            <li className={styles.subNavLinkItems}>
+              <Link href="/services/lightening">Lighting</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="">EV Chargers</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Electric Services / Maintenance / Upgrading</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Automation</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">IT Solutions / Networking / Wi-Fi</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#"> Conference Room</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">LED Lights Maintenance</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Access Control</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Audio Visual</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Security</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#"> Temperature Control</Link>
+            </li>
+          </ul>
+        )}
 
-
-        <li className={`${styles.list} ${styles.navLink} my-[40px]`}
-         onMouseEnter={handleOnMouseEnterResidancial}
-         onMouseLeave={handleOnMouseLeaveResidancial}>
-       
-          <Link href="#" className={`${fonts.prodPageHeroTitle} ${styles.listTitleResidencial} `}>
+        <li
+          className={`${styles.list} ${styles.navLink} mt-10`}
+          onMouseEnter={handleOnMouseEnterResidancial}
+          onMouseLeave={handleOnMouseLeaveResidancial}
+        >
+          <Link
+            href="#"
+            className={`${fonts.prodPageHeroTitle} ${styles.listTitleResidencial} `}
+          >
             Residencial
             <Image
               src={arrow}
@@ -92,28 +137,52 @@ const Services = () => {
               className={`mt-[4px] ${styles.arrow}`}
             />
           </Link>
-          {isSubMenuResidencial && (
-            <ul className={`${styles.subNavLink} ${isHovered ? styles.hovered : ''}`}>
-              <li className={styles.subNavLinkItems}>
-                <Link href="/services/lightening">Lighting</Link>
-              </li>
-              <li className={styles.subNavLinkItems}>
-                <Link href="#">Menu-2</Link>
-              </li>
-              <li className={styles.subNavLinkItems}>
-                <Link href="#">Menu-3</Link>
-              </li>
-              <li className={styles.subNavLinkItems}>
-                <Link href="#">Menu-4</Link>
-              </li>
-            </ul>
-          )}
         </li>
+        {isSubMenuResidencial && (
+          <ul
+            className={`${styles.subNavLink}`}
+            onMouseEnter={handleOnMouseEnterResidancial}
+            //  onMouseLeave={handleOnMouseLeaveResidancial}
+          >
+            <li className={styles.subNavLinkItems}>
+              <Link href="/services/lightening">Custom Home Automation</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="">EV Security</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">IT Solutions / EV Chargers</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Home Cinema or Home Theater</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Electrical Design / Study</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Integrated Structured Wiring</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Multi room Sound System</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Temperature Control</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Audio Visual</Link>
+            </li>
+          </ul>
+        )}
 
-
-        <li className={`${styles.list}`}>
-         
-          <Link href="" className={`${fonts.prodPageHeroTitle} ${styles.listTitleSmart} `}>
+        <li
+          className={`${styles.list} mt-10`}
+          onMouseEnter={handleOnMouseEnterAutomation}
+          onMouseLeave={handleOnMouseLeaveAutomation}
+        >
+          <Link
+            href=""
+            className={`${fonts.prodPageHeroTitle} ${styles.listTitleSmart} `}
+          >
             Smart Automations
             <Image
               src={arrow}
@@ -124,6 +193,41 @@ const Services = () => {
             />
           </Link>
         </li>
+        {isSubMenuAuto && (
+          <ul
+            className={`${styles.subNavLink}`}
+            onMouseEnter={handleOnMouseEnterAutomation}
+             onMouseLeave={handleOnMouseLeaveAutomation}
+          >
+            <li className={styles.subNavLinkItems}>
+              <Link href="/services/lightening">Custom Home Automation</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="">EV Security</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">IT Solutions / EV Chargers</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Home Cinema or Home Theater</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Electrical Design / Study</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Integrated Structured Wiring</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Multi room Sound System</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Temperature Control</Link>
+            </li>
+            <li className={styles.subNavLinkItems}>
+              <Link href="#">Audio Visual</Link>
+            </li>
+          </ul>
+        )}
       </ul>
     </>
   );
