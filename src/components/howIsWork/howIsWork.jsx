526d58377f319d3d884608130830e0616a89e958
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-01-29 14:31:32 +0200

    doubleArrow

--- src/components/howIsWork/howIsWork.jsx
+++ src/components/howIsWork/howIsWork.jsx
@@ -1,35 +1,52 @@
-"use client"
-import { useState } from "react"
-import list from "../../../public/json/howIsWork.json"
-import fonts from "../fonts/fonts.module.scss"
+"use client";
+import React, { useState } from "react";
+import list from "../../../public/json/howIsWork.json";
+import fonts from "../fonts/fonts.module.scss";
 
+import doubleArrow from "../../../public/icon/doubleArrow.png";
 
-import Image from "next/image"
+import Image from "next/image";
 
 const HowIsWork = () => {
+  const [data, setData] = useState(list);
 
-const [data, setData] = useState(list)
+  return (
+    <>
+    <h1 className={`${fonts.productCardTitle} text-start mt-[120px] mb-[40px]`}>
+      How does it work{" "}
+    </h1>
+    <ul className="flex flex-row justify-around items-center ">
+      {data.map((item, index) => (
+        <React.Fragment key={item.title}>
+          <li
+            className={`flex flex-col px-[24px] bg-[#292D36] w-[348px] border rounded-md ${
+              index !== data.length - 1 ? 'mb-[20px]' : ''
+            }`}
+          >
+            <Image
+              src={item.icon}
+              alt="icon"
+              width={160}
+              height={160}
+              className="pt-[20px] mx-auto hover:scale-110"
+            />
+            <p className={`${fonts.howIsWorkTitle} mx-auto py-[24px]`}>
+              {item.title}
+            </p>
+            <p className={`${fonts.howIsWorkScript} mx-auto w-[300px] h-[130px]`}>
+              {item.script}
+            </p>
+          </li>          
+          {index !== data.length - 1 && (
+            <li className="flex items-center">
+              <Image src={doubleArrow} alt="doubleArrow" width={76} height={76} />
+            </li>
+          )}
+        </React.Fragment>
+      ))}
+    </ul>
+  </>
+  );
+};
 
-    return (
-        <>
-        <h1 className={`${fonts.productCardTitle} text-start mt-[120px] mb-[40px]`}>How does it work  </h1>
-        <ul className="flex flex-row justify-around items-center ">
-            {data.map((item) => (
-                <li key={item.title} className=" flex flex-col px-[24px] bg-[#292D36] w-[348px] border rounded-md">
-                    <Image 
-                    src={item.icon}
-                    alt="icon"
-                    width={160}
-                    height={160}
-                    className="pt-[20px] mx-auto hover:scale-110"
-                    />
-                    <p className={`${fonts.howIsWorkTitle} mx-auto py-[24px]`}>{item.title}</p>
-                    <p className={`${fonts.howIsWorkScript} mx-auto w-[300px] h-[130px]`}>{item.script}</p>
-                </li>
-            ))}            
-        </ul>
-        </>
-    )
-}
-
-export default HowIsWork
+export default HowIsWork;
